d4f4d057191e516f637d0489e58a0b41bd31b8eb
electrodrigo <rodrigo.carrasco.davis@gmail.com>
2018-05-07 17:47:12 -0300

    name by lightcurves

--- image_simulation/config_images.py
+++ image_simulation/config_images.py
@@ -5,10 +5,16 @@
 # galaxy_path = "/home/rodrigo/supernovae_detection/galaxies_guille/gal_mags_dev_exp_z_all_Filter_rodrigocd.csv"
 # lightcurves_path = "/home/toshiba/rodrigo/simulated_lightcurves/HiTS_lc/hits_2500"
 # Personal
-#save_path = "/simulated_data/" # System directory with more space
-save_path = "/home/rodrigo/supernovae_detection/simulated_data/image_sequences/"
-galaxy_path = "/home/rodrigo/supernovae_detection/galaxies/gal_mags_dev_exp_z_all_Filter_rodrigocd.csv"
-lightcurves_path = "/home/rodrigo/supernovae_detection/simulated_data/lightcurves/hits_30"
+# save_path = "/simulated_data/" # System directory with more space
+# save_path = "/home/rodrigo/supernovae_detection/simulated_data/image_sequences/"
+# galaxy_path = "/home/rodrigo/supernovae_detection/galaxies/gal_mags_dev_exp_z_all_Filter_rodrigocd.csv"
+# lightcurves_path = "/home/rodrigo/supernovae_detection/simulated_data/lightcurves/hits_30"
+# Cluster
+save_path = "/home/rcarrasco/simulated_data/image_sequences/"
+galaxy_path = "/home/rcarrasco/simulated_data/galaxies/gal_mags_dev_exp_z_all_Filter_rodrigocd.csv"
+lightcurves_path = "/home/rcarrasco/simulated_data/lightcurves/"
+lightcurve_name = "brighter_hits_2500"
+lightcurves_path = lightcurves_path + lightcurve_name
 
 # Configuration Variables
 requested_lightcurve = ["Supernovae", "RRLyrae", "M33Cepheids", "EclipsingBinaries",
@@ -17,10 +23,11 @@ requested_lightcurve_labels = [0, 1, 2, 3, 4, 5, 6]  # multiclass
 bands = ["g", ]
 stamp_size = (21, 21)
 proportion_with_galaxy = [0.5, 0.05, 0.05, 0.05, 0.05, 1.0, 0]
-lc_per_chunk = 10
+lc_per_chunk = 2000
 sky_clipping = 2000
 astrometric_error = 0.3
-output_filename = "small_set_tfrecord"
+output_filename = "complete_set"
+output_filename = output_filename + "_" + lightcurve_name
 
 # Don't worry about this
 camera_and_obs_cond_path = "../real_obs/pickles/camera_and_obs_cond.pkl"
